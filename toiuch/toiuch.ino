49113bb32f35bdbd892ac4cb1c032b8488a1281a
ritvik69 <128372686+ritvik69@users.noreply.github.com>
2023-03-29 15:04:28 +0530

    added treshold value of 60 bpm

--- toiuch/toiuch.ino
+++ toiuch/toiuch.ino
@@ -1,24 +1,41 @@
-// constants won't change. They're used here to set pin numbers:
-const int SENSOR_PIN = 7; // the Arduino's input pin that connects to the sensor's SIGNAL pin 
+#define USE_ARDUINO_INTERRUPTS true  // Set-up low-level interrupts for most acurate BPM math.
+#include <PulseSensorPlayground.h>   // Includes the PulseSensorPlayground Library.
 
-// Variables will change:
-int lastState = LOW;      // the previous state from the input pin
-int currentState;         // the current reading from the input pin
 
+// Variables
+const int PulseWire = 0;  // PulseSensor PURPLE WIRE connected to ANALOG PIN 0
+const int LED13 = 13;     // The on-board Arduino LED, close to PIN 13.
+int Threshold = 550;      // Determine which Signal to "count as a beat" and which to ignore.
+// Use the "Gettting Started Project" to fine-tune Threshold Value beyond default setting.
+// Otherwise leave the default "550" value.
+
+PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-  // initialize the Arduino's pin as aninput
-  pinMode(SENSOR_PIN, INPUT);
+
+  Serial.begin(9600);  // For Serial Monitor
+
+
+  // Configure the PulseSensor object, by assigning our variables to it.
+  pulseSensor.analogInput(PulseWire);
+  pulseSensor.blinkOnPulse(LED13);  //auto-magically blink Arduino's LED with heartbeat.
+  pulseSensor.setThreshold(Threshold);
+
+  // Double-check the "pulseSensor" object was created and "began" seeing a signal.
+  if (pulseSensor.begin()) {
+    Serial.println("We created a pulseSensor Object !");  //This prints one time at Arduino power-up, or on Arduino reset.
+  }
 }
 
 void loop() {
-  // read the state of the the input pin:
-  currentState = digitalRead(SENSOR_PIN);
 
-  if(lastState == LOW && currentState == HIGH)
-    Serial.println("The sensor is touched");
-
-  // save the the last state
-  lastState = currentState;
+  int myBPM = pulseSensor.getBeatsPerMinute();    // Calls function on our pulseSensor object that returns BPM as an "int".
+  if (pulseSensor.sawStartOfBeat()) {             // Constantly test to see if "a beat happened".
+    Serial.println("♥ A HeartBeat Happened ! ");  // If test is "true", print a message "a heartbeat happened".
+    Serial.print("BPM: ");                        // Print phrase "BPM: "
+    Serial.println(myBPM);                        // Print the value inside of myBPM.
+    if (myBPM < 60) {
+      Serial.print("UR DROWSY ");
+    }
+  }
+  delay(2000);  // considered best practice in a simple sketch.
 }
