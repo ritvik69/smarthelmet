f719c4cf2ad703247fa25f000bdea9c4ce74ef49
ritvik69 <128372686+ritvik69@users.noreply.github.com>
2023-03-31 14:00:18 +0530

    added accident detection code

---
+++ accident_detection/accident_detection.ino
@@ -0,0 +1,111 @@
+
+
+#define x A1
+
+#define y A2
+
+#define z A3
+
+
+int xsample = 0;
+
+int ysample = 0;
+
+int zsample = 0;
+
+
+#define samples 10
+
+#define minVal -50
+
+#define MaxVal 50
+
+
+int i = 0, k = 0;
+
+
+
+
+void setup()
+
+{
+
+
+  Serial.begin(9600);
+
+
+  for (int i = 0; i < samples; i++)
+
+  {
+
+    xsample += analogRead(x);
+
+    ysample += analogRead(y);
+
+    zsample += analogRead(z);
+  }
+
+
+  xsample /= samples;
+
+  ysample /= samples;
+
+  zsample /= samples;
+
+
+  Serial.println(xsample);
+
+  Serial.println(ysample);
+
+  Serial.println(zsample);
+
+  delay(5000);
+
+
+}
+
+
+void loop()
+
+{
+
+  int value1 = analogRead(x);
+
+  int value2 = analogRead(y);
+
+  int value3 = analogRead(z);
+
+
+  int xValue = xsample - value1;
+
+  int yValue = ysample - value2;
+
+  int zValue = zsample - value3;
+
+
+
+  Serial.print("x=");
+
+  Serial.println(xValue);
+
+  Serial.print("y=");
+
+  Serial.println(yValue);
+
+  Serial.print("z=");
+
+  Serial.println(zValue);
+  
+  delay(1000);
+
+  if (xValue < minVal || xValue > MaxVal || yValue < minVal || yValue > MaxVal || zValue < minVal || zValue > MaxVal)
+
+  {
+
+      Serial.print("Accident detected \n");
+  }
+  
+}
+
+
+
