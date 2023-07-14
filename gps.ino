c2381a6e5e0d910f497f0fca75c2db8c669a94f5
ritvik69 <128372686+ritvik69@users.noreply.github.com>
2023-07-14 15:35:16 +0530

    Update gps.ino

--- gps.ino
+++ gps.ino
@@ -53,40 +53,6 @@ void displayInfo()
   {
     Serial.println("Location: Not Available");
   }
-  
-  Serial.print("Date: ");
-  if (gps.date.isValid())
-  {
-    Serial.print(gps.date.month());
-    Serial.print("/");
-    Serial.print(gps.date.day());
-    Serial.print("/");
-    Serial.println(gps.date.year());
-  }
-  else
-  {
-    Serial.println("Not Available");
-  }
-
-  Serial.print("Time: ");
-  if (gps.time.isValid())
-  {
-    if (gps.time.hour() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.hour());
-    Serial.print(":");
-    if (gps.time.minute() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.minute());
-    Serial.print(":");
-    if (gps.time.second() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.second());
-    Serial.print(".");
-    if (gps.time.centisecond() < 10) Serial.print(F("0"));
-    Serial.println(gps.time.centisecond());
-  }
-  else
-  {
-    Serial.println("Not Available");
-  }
 
   Serial.println();
   Serial.println();
